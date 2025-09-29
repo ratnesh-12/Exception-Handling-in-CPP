db798dceb414e7cac376f50ac3f28d5612ade53e
Ratnesh Kumar <ratneshkr129@gmail.com>
2025-09-29 21:51:27 +0530

    Create Access Code.cpp

---
+++ Access Code.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double code;
+    cout << "Enter the private code:";
+    cin >> code;
+    try{
+        if(code!=9762){
+            throw code;
+        }
+        else{
+            cout << "Access Granted" << endl;
+        }
+    }
+    catch(double wrongCode){
+        cout << "\nERROR: Entered wrong code"<<endl;
+    }
+    return 0;
+}
+
+OUTPUT:
+
+Enter the private code:9762
+Access Granted
